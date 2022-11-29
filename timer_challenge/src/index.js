18eef66ae356e03887eb045925342b4ef3930f06
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-29 15:55:09 +0000

    add global variables

---
+++ timer_challenge/src/index.js
@@ -0,0 +1,5 @@
+// Global variables 
+const timer = document.querySelector('#timer');
+const start = document.querySelector('#start');
+const stop = document.querySelector('#stop');
+const reset = document.querySelector('#reset');
