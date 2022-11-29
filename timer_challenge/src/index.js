719b9684d67c8c913603d6da33942010b117b5ef
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-29 15:57:28 +0000

    add seconds and interval variables

--- timer_challenge/src/index.js
+++ timer_challenge/src/index.js
@@ -3,3 +3,6 @@ const timer = document.querySelector('#timer');
 const start = document.querySelector('#start');
 const stop = document.querySelector('#stop');
 const reset = document.querySelector('#reset');
+
+let seconds = 0;
+let interval = null;
