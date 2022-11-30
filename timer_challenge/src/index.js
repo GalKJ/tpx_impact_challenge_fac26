bb638cb6e29b878c0b3880e1aaedfd2142666b1b
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-30 15:19:44 +0000

    add sudo code

--- timer_challenge/src/index.js
+++ timer_challenge/src/index.js
@@ -1,29 +1,28 @@
-// Global variables 
+// Global variables. 
     const timeEl = document.querySelector('#timer');
     const startButton = document.querySelector('#start');
     const stopButton = document.querySelector('#stop');
     const resetButton = document.querySelector('#reset');
     const lapButton = document.querySelector('#lap');
-    const lapList = document.querySelector('#lap-times');
+    const lapList = document.querySelector('#lap-list');
 
 
     let centiSeconds = 0;
     let interval = null;
 
-// Event listeners
+// Event listeners.
     startButton.addEventListener('click', start);
     stopButton.addEventListener('click', stop);
     resetButton.addEventListener('click', reset);
     lapButton.addEventListener('click', lap);
 
-
+// Function to define hrs, mins, secs and centi, increment the centiseconds and render the variable values to the timeEl variables inner text.
     function timer () {
         centiSeconds ++;
 
         let hrs = Math.floor(centiSeconds / 360000);
         let mins = Math.floor((centiSeconds - (hrs * 360000)) / 6000);
         let secs = Math.floor((centiSeconds - (mins * 6000)) / 100);
-        // let secs = centiSeconds / 100;
         let centi = centiSeconds % 100;
 
         if (centi < 10) centi = '0' + centi;
@@ -34,6 +33,7 @@
         timeEl.innerText = `${hrs}:${mins}:${secs}:${centi}`;
     }
 
+// Function to run the timer function at a set interval of 10 milliseconds (1 centisecond).
     function start () {
         if (interval) {
             return
@@ -42,17 +42,21 @@
         interval = setInterval(timer, 10);
     }
 
+// Function to stop the timer function running by clearing the interval.
     function stop () {
         clearInterval(interval);
         interval = null;
     }
 
+// Function to run the stop function, reset the centiseconds to 0, the timeEl inner text to the DOM value and the lapList inner HTML to an empty paragraph.
     function reset () {
         stop();
         centiSeconds = 0;
         timeEl.innerText = '00:00:00:00';
+        lapList.innerHTML = `<p></p>`;
     }
 
+// Function to create a new paragraph, render the current timeEl value as it's inner text and append it to the lapList div. 
     function lap () {
         const lap = document.createElement('p');
         lap.textContent = timeEl.innerText;
