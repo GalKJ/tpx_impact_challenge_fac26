5b6eb766aab69e3ff5a2995de38c5c9295e1b442
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-30 14:08:20 +0000

    add centi if statement and secs math floor

--- timer_challenge/src/index.js
+++ timer_challenge/src/index.js
@@ -13,18 +13,28 @@
     resetButton.addEventListener('click', reset);
 
 
-    function timer() {
+    function timer () {
         centiSeconds ++;
 
         let hrs = Math.floor(centiSeconds / 360000);
         let mins = Math.floor((centiSeconds - (hrs * 360000)) / 6000);
-        let secs = centiSeconds / 100;
+        let secs = Math.floor((centiSeconds - (mins * 6000)) / 100);
+        // let secs = centiSeconds / 100;
         let centi = centiSeconds % 100;
 
-        if (centi < 100) centi = '00' + centi;
+        if (centi < 10) {
+            centi = '00' + centi
+        } else if (centi > 9) {
+            centi = '0' + centi 
+        }
+
         if (secs < 10) secs = '0' + secs;
         if (mins < 10) mins = "0" + mins;
         if (hrs < 10) hrs = "0" + hrs;
+
+        // console.log(centiSeconds);
+        // console.log(centi);
+        // console.log(secs);
 
         timeEl.innerText = `${hrs}:${mins}:${secs}:${centi}`;
     }
@@ -34,7 +44,7 @@
             return
         }
 
-        interval = setInterval(timer, 1000);
+        interval = setInterval(timer, 10);
     }
 
     function stop () {
