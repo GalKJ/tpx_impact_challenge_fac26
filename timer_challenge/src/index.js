66487318d4eeed3303fd252b8af3a14baeb87749
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-30 14:14:35 +0000

    refactor centi if statement

--- timer_challenge/src/index.js
+++ timer_challenge/src/index.js
@@ -22,19 +22,10 @@
         // let secs = centiSeconds / 100;
         let centi = centiSeconds % 100;
 
-        if (centi < 10) {
-            centi = '00' + centi
-        } else if (centi > 9) {
-            centi = '0' + centi 
-        }
-
+        if (centi < 10) centi = '0' + centi;
         if (secs < 10) secs = '0' + secs;
         if (mins < 10) mins = "0" + mins;
         if (hrs < 10) hrs = "0" + hrs;
-
-        // console.log(centiSeconds);
-        // console.log(centi);
-        // console.log(secs);
 
         timeEl.innerText = `${hrs}:${mins}:${secs}:${centi}`;
     }
@@ -55,5 +46,5 @@
     function reset () {
         stop();
         centiSeconds = 0;
-        timeEl.innerText = '00:00:00:000';
+        timeEl.innerText = '00:00:00:00';
     }
