7b16a5f97e554ede47939e3f348ff057fc412aa7
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-30 11:08:20 +0000

    add timer function

--- timer_challenge/src/index.js
+++ timer_challenge/src/index.js
@@ -1,8 +1,22 @@
 // Global variables 
-const timer = document.querySelector('#timer');
+const timerEl = document.querySelector('#timer');
 const start = document.querySelector('#start');
 const stop = document.querySelector('#stop');
 const reset = document.querySelector('#reset');
 
 let seconds = 0;
 let interval = null;
+
+function timer() {
+    seconds ++;
+
+    let hrs = Math.floor(seconds / 3600);
+    let mins = Math.floor((seconds - (hrs * 3600)) / 60);
+    let secs = seconds % 60;
+
+    if (secs < 10) secs = '0' + secs;
+	if (mins < 10) mins = "0" + mins;
+	if (hrs < 10) hrs = "0" + hrs;
+
+	timeEl.innerText = `${hrs}:${mins}:${secs}`;
+}
