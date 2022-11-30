739989de349f4db004765a4d26da9053d4d4a32b
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-11-30 11:20:12 +0000

    add start function and centisecond let

--- timer_challenge/src/index.js
+++ timer_challenge/src/index.js
@@ -1,22 +1,35 @@
 // Global variables 
-const timerEl = document.querySelector('#timer');
-const start = document.querySelector('#start');
-const stop = document.querySelector('#stop');
-const reset = document.querySelector('#reset');
+    const timeEl = document.querySelector('#timer');
+    const startButton = document.querySelector('#start');
+    const stopButton = document.querySelector('#stop');
+    const resetButton = document.querySelector('#reset');
 
-let seconds = 0;
-let interval = null;
+    let seconds = 0;
+    let interval = null;
 
-function timer() {
-    seconds ++;
+// Event listeners
+    startButton.addEventListener('click', start);
 
-    let hrs = Math.floor(seconds / 3600);
-    let mins = Math.floor((seconds - (hrs * 3600)) / 60);
-    let secs = seconds % 60;
 
-    if (secs < 10) secs = '0' + secs;
-	if (mins < 10) mins = "0" + mins;
-	if (hrs < 10) hrs = "0" + hrs;
+    function timer() {
+        seconds ++;
 
-	timeEl.innerText = `${hrs}:${mins}:${secs}`;
-}
+        let hrs = Math.floor(seconds / 3600);
+        let mins = Math.floor((seconds - (hrs * 3600)) / 60);
+        let secs = seconds % 60;
+        let centi = seconds * 100;
+
+        if (secs < 10) secs = '0' + secs;
+        if (mins < 10) mins = "0" + mins;
+        if (hrs < 10) hrs = "0" + hrs;
+
+        timeEl.innerText = `${hrs}:${mins}:${secs}:${centi}`;
+    }
+
+    function start () {
+        if (interval) {
+            return
+        }
+
+        interval = setInterval(timer, 1000);
+    }
